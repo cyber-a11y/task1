35a6e13170816705a03d81e1d655ffe2320fba85
denyzabra <denyzabrahams0@gmail.com>
2023-09-29 20:11:16 +0300

    here is some code to start from

---
+++ script.js
@@ -0,0 +1,18 @@
+// Get references to the input field and the button by their IDs
+var textInput = document.getElementById("textInput");
+var showAlertButton = document.getElementById("showAlertButton");
+
+    // Add a click event listener to the button
+    //add code below
+
+    // Get the value entered in the input field
+    //add code below
+  
+
+    // Display an alert with the entered text
+    //add code below
+   
+
+    // Clear the input field
+    //add code below
+ 
