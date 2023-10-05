8e03102db5ad29d801a8c1782fe02492ab215acf
cyber-ally <rosesaredeadly670@gmail.com>
2023-10-04 20:43:15 -0500

    tried

--- script.js
+++ script.js
@@ -2,16 +2,22 @@
 var textInput = document.getElementById("textInput");
 var showAlertButton = document.getElementById("showAlertButton");
 
+
     // Add a click event listener to the button
     //add code below
-
+showAlertButton.addEventListener("click", function() {alert("Alert");});
+ 
     // Get the value entered in the input field
     //add code below
-  
+ {
+        var textinput = document.getElementById("textinput").value;
+        document.getElementById("showAlertButton").innerHTML = showAlertButtons;
+    }
 
     // Display an alert with the entered text
     //add code below
-   
+    var showAlertButton = document.getElementById("showAlertButton").innerHTML;
+   alert('innerHTNL:' +showAlertButton);
 
     // Clear the input field
     //add code below
